8694b063a3df66b7e37b996c7b6c4ebfb7ea4a21
Wimpi18 <winsoromar12@gmail.com>
2024-10-16 11:17:28 -0400

    feat: datos iniciales para inicializar

--- src/components/Aflatoxinas.tsx
+++ src/components/Aflatoxinas.tsx
@@ -5,16 +5,22 @@ const Aflatoxinas = () => {
     <div className="flex justify-center items-center w-full h-full">
       <div className="w-full max-w-4xl h-full max-h-96">
         <BarChart
-          xAxis={[{ scaleType: "band", data: ["B1", "B2", "G1", "G2"], label:"Tipo de Aflatoxina" }]}
-          yAxis={[{label:"μg/Kg"}]}
+          xAxis={[
+            {
+              scaleType: "band",
+              data: ["B1", "B2", "G1", "G2"],
+              label: "Tipo de Aflatoxina",
+            },
+          ]}
+          yAxis={[{ label: "μg/Kg" }]}
           series={[
             {
               label: "Antes del vuelo",
-              data: [100, 80, 43, 30],
+              data: [0, 0, 0, 0],
             },
             {
               label: "Después del vuelo",
-              data: [10, 12, 20, 11],
+              data: [0, 0, 0, 0],
             },
           ]}
           title="Aflatoxinas en pasta de castaña"
